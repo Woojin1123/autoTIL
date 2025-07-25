b4cdfac1a971d0d9e0cbf7b4bd20c74393f166bb
Woojin1123 <boom175839@gmail.com>
2025-07-25 19:29:15 +0900

    init commit

---
+++ src/test/java/com/woojin/autotil/AutoTilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woojin.autotil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
