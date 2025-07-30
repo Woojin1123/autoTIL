5e6dd3b64dbd34b79a72e07cf2a5c7b0429c2019
Woojin1123 <boom175839@gmail.com>
2025-07-31 00:03:49 +0900

    refactor : filter JWT 인증 cookie로 변경 (#1)

--- src/main/java/com/woojin/autotil/security/filter/JwtFilter.java
+++ src/main/java/com/woojin/autotil/security/filter/JwtFilter.java
@@ -1,14 +1,15 @@
 package com.woojin.autotil.security.filter;
 
 import com.woojin.autotil.common.util.JwtUtil;
-import com.woojin.autotil.user.dto.AuthUser;
-import com.woojin.autotil.user.enums.Role;
+import com.woojin.autotil.auth.dto.AuthUser;
+import com.woojin.autotil.auth.enums.Role;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -35,65 +36,58 @@ public class JwtFilter extends OncePerRequestFilter {
             FilterChain filterChain)
             throws ServletException, IOException {
 
-        String requestURI = request.getRequestURI();
+        String jwt = extractTokenFromCookie(request);
 
-        if (isPublicPath(requestURI)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        String authorizationHeader = request.getHeader("Authorization");
-
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            String jwt = jwtUtil.substringToken(authorizationHeader);
-
+        if (jwt != null) {
             try {
                 Claims claims = jwtUtil.extractClaims(jwt);
-                if (claims == null) {
-                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "잘못된 JWT 토큰입니다.");
-                    return;
-                }
                 Long githubId = Long.valueOf(claims.getSubject());
                 String loginId = String.valueOf(claims.get("loginId"));
                 Role role = Role.of(String.valueOf(claims.get("role")));
 
-                if(SecurityContextHolder.getContext().getAuthentication() == null){
-                    AuthUser authUser = AuthUser.builder()
-                            .githubId(githubId)
-                            .loginId(loginId)
-                            .role(role)
-                            .build();
-                    UsernamePasswordAuthenticationToken authentication =
-                            new UsernamePasswordAuthenticationToken(authUser, null, Collections.singletonList(new SimpleGrantedAuthority(role.name())));
+                AuthUser authUser = AuthUser.builder()
+                        .githubId(githubId)
+                        .loginId(loginId)
+                        .role(role)
+                        .build();
 
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                }
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                authUser, null,
+                                Collections.singletonList(new SimpleGrantedAuthority(role.name()))
+                        );
 
+                SecurityContextHolder.getContext().setAuthentication(authentication);
 
             } catch (SecurityException | MalformedJwtException e) {
-                log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.", e);
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않는 JWT 서명입니다.");
+                log.error("Invalid JWT signature", e);
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않은 JWT 서명입니다.");
+                return;
             } catch (ExpiredJwtException e) {
-                log.error("Expired JWT token, 만료된 JWT token 입니다.", e);
+                log.error("Expired JWT token", e);
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "만료된 JWT 토큰입니다.");
+                return;
             } catch (UnsupportedJwtException e) {
-                log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.", e);
+                log.error("Unsupported JWT token", e);
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST, "지원되지 않는 JWT 토큰입니다.");
+                return;
             } catch (Exception e) {
-                log.error("Invalid JWT token, 유효하지 않는 JWT 토큰 입니다.", e);
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "유효하지 않는 JWT 토큰입니다.");
+                log.error("JWT 처리 실패", e);
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "JWT 토큰 처리 중 오류가 발생했습니다.");
+                return;
             }
         }
         filterChain.doFilter(request, response);
     }
 
-    private boolean isPublicPath(String path) {
-        return path.startsWith("/api/v1/auth") ||
-                path.equals("/v3/api-docs") ||
-                path.startsWith("/v3/api-docs/") ||
-                path.startsWith("/swagger-ui/") ||
-                path.equals("/swagger-ui.html") ||
-                path.startsWith("/swagger-resources") ||
-                path.startsWith("/webjars");
+    private String extractTokenFromCookie(HttpServletRequest request) {
+        if (request.getCookies() == null) return null;
+
+        for (Cookie cookie : request.getCookies()) {
+            if ("access-token".equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return null;
     }
 }
