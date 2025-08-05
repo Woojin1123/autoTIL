ca95ce588b6afd5628c053be85b7319d325ef5d9
Woojin1123 <boom175839@gmail.com>
2025-08-05 11:48:10 +0900

    refactor: security 인증객체 사용하도록 변경

--- src/main/java/com/woojin/autotil/github/controller/GithubController.java
+++ src/main/java/com/woojin/autotil/github/controller/GithubController.java
@@ -2,15 +2,13 @@ package com.woojin.autotil.github.controller;
 
 import com.woojin.autotil.common.response.ApiResponse;
 import com.woojin.autotil.github.dto.GithubRepoResponse;
+import com.woojin.autotil.github.dto.repoTrackRequest;
 import com.woojin.autotil.github.service.GithubService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,15 +21,26 @@ public class GithubController {
     private final GithubService githubService;
 
     @GetMapping("/repos")
-    public ResponseEntity<ApiResponse<List<GithubRepoResponse>>> getRepositories(
-            @CookieValue("access_token") String accessToken
-    ) {
+    public ResponseEntity<ApiResponse<List<GithubRepoResponse>>> getRepositories() {
         return ResponseEntity.ok(
                 ApiResponse.success(
                         200,
                         HttpStatus.OK,
                         "레포지토리 로딩 성공",
-                        githubService.getRepositories(accessToken)
+                        githubService.getRepositories()
+                ));
+    }
+
+    @PatchMapping("/repos")
+    public ResponseEntity<ApiResponse<List<Long>>> updateRepoTracked(
+            @RequestBody repoTrackRequest request
+    ) {
+        return ResponseEntity.ok(
+                ApiResponse.success(
+                        200,
+                        HttpStatus.OK,
+                        "레포지토리 업데이트 성공",
+                        githubService.updateRepoTracked(request)
                 ));
     }
 
